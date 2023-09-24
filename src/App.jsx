99f9e99a29a3d46bfab5e2bfcf34702da6716b5e
박준현 <me@4rne5.dev>
2023-09-24 19:56:46 +0900

    Finish?:Darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+
 import profile from './assets/ppr.svg'
 import notionlogo from './assets/notion.svg'
 import instalogo from './assets/instagram2.svg'
@@ -7,9 +10,31 @@ import moneylogo from './assets/money5.svg'
 import telegramlogo from './assets/telegram.svg'
 import linkedinlogo from './assets/linkedin2.svg'
 import discordlogo from './assets/discord.svg'
-import './App.css'
+
+import notiondark from './assetdark/notion.svg'
+import instadark from './assetdark/instagram.svg'
+import githubdark from './assetdark/github.svg'
+import maildark from './assetdark/mail.svg'
+import moneydark from './assetdark/money.svg'
+import telegramdark from './assetdark/telegram.svg'
+import linkedindark from './assetdark/linkedin.svg'
+import discorddark from './assetdark/discord.svg'
+
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const matcher = window.matchMedia('(prefers-color-scheme: dark)');
+    setIsDarkMode(matcher.matches);
+    matcher.addEventListener('change', (e) => setIsDarkMode(e.matches));
+
+    // cleanup function
+    return () => {
+      matcher.removeEventListener('change', (e) => setIsDarkMode(e.matches));
+    };
+  }, []);
+
   return (
     <div className="background-image">
     <>
@@ -22,25 +47,25 @@ function App() {
         <div className="fstline">
           <div className="logo-container">
             <a href="https://www.instagram.com/4_rne5/" target="_blank" rel="noreferrer">
-              <img src={instalogo} className="logo" alt="Insta logo" />
+              <img src={isDarkMode ? instadark : instalogo} className="logo" alt="Insta logo" />
             </a>
             <span className="logo-text">Instagram</span>
           </div>
           <div className="logo-container">
             <a href="https://www.discord.com/users/796907962811285506" target="_blank" rel="noreferrer">
-              <img src={discordlogo} className="logo" alt="discord logo" />
+              <img src={isDarkMode ? discorddark : discordlogo} className="logo" alt="discord logo" />
             </a>
             <span className="logo-text">Mail</span>
           </div>
         <div className="logo-container">
         <a href="https://github.com/4rNe5" target="_blank" rel="noreferrer">
-          <img src={githublogo} className="logo" alt="Github logo" />
+          <img src={isDarkMode ? githubdark : githublogo} className="logo" alt="Github logo" />
         </a>
           <span className="logo-text">Github</span>
         </div>
           <div className="logo-container hidem">
             <a href="https://t.me/A4rne5" target="_blank" rel="noreferrer">
-              <img src={telegramlogo} className="logo" alt="Telegram logo" />
+              <img src={isDarkMode ? telegramdark : telegramlogo} className="logo" alt="Telegram logo" />
             </a>
             <span className="logo-text">Mail</span>
           </div>
@@ -48,25 +73,25 @@ function App() {
         <div>
           <div className="logo-container">
             <a href="http://4rne5-portfolio.kro.kr" target="_blank" rel="noreferrer">
-              <img src={notionlogo} className="logo" alt="Notion logo" />
+              <img src={isDarkMode ? notiondark : notionlogo} className="logo" alt="Notion logo" />
             </a>
             <span className="logo-text">Notion</span>
           </div>
           <div className="logo-container">
             <a href="mailto:me@4rne5.dev" target="_blank" rel="noreferrer">
-              <img src={maillogo} className="logo" alt="Mail logo" />
+              <img src={isDarkMode ? maildark : maillogo} className="logo" alt="Mail logo" />
             </a>
             <span className="logo-text">Mail</span>
           </div>
           <div className="logo-container">
             <a href="https://toss.me/4rne5" target="_blank" rel="noreferrer">
-              <img src={moneylogo} className="logo" alt="Money logo" />
+              <img src={isDarkMode ? moneydark : moneylogo} className="logo" alt="Money logo" />
             </a>
             <span className="logo-text">Mail</span>
           </div>
           <div className="logo-container">
             <a href="https://www.linkedin.com/in/4rNe5" target="_blank" rel="noreferrer">
-              <img src={linkedinlogo} className="logo" alt="linkedin logo" />
+              <img src={isDarkMode ? linkedindark : linkedinlogo} className="logo" alt="linkedin logo" />
             </a>
             <span className="logo-text">Mail</span>
           </div>
